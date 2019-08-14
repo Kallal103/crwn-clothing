123b04b8605218d79fdb1f32a24657004606fe89
high5digital <kallal.nsu103@gmail.com>
2019-08-15 00:26:16 +0600

    add files for homepages

---
+++ src/homepage.component.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+        <div className='menu-item'>
+         <div className='content'>
+            <h1 className='title'>Hats</h1>
+            <span className='subtitle'>Shop Now</span>
+
+         </div>
+        </div>
+        <div className='menu-item'>
+         <div className='content'>
+            <h1 className='title'>JACKETS</h1>
+            <span className='subtitle'>Shop Now</span>
+
+         </div>
+        </div>
+        <div className='menu-item'>
+         <div className='content'>
+            <h1 className='title'>SNEAKERS</h1>
+            <span className='subtitle'>Shop Now</span>
+
+         </div>
+        </div>
+        <div className='menu-item'>
+         <div className='content'>
+            <h1 className='title'>WOMENS</h1>
+            <span className='subtitle'>Shop Now</span>
+
+         </div>
+        </div>
+        <div className='menu-item'>
+         <div className='content'>
+            <h1 className='title'>MENS</h1>
+            <span className='subtitle'>Shop Now</span>
+
+         </div>
+        </div>
+        </div>
+
+    </div>
+)
+
+export default HomePage;
